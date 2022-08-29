ea06f747b701f2a1bf758bb650376c96beb37a34
romee <romyoh@gmail.com>
2022-08-29 16:23:34 +0900

    초기 커밋.

---
+++ WebApplication4/Window1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WebApplication4
+{
+    partial class Window1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new Wisej.Web.TextBox();
+            this.label1 = new Wisej.Web.Label();
+            this.button1 = new Wisej.Web.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cookie value";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save cookie";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 201);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Window1";
+            this.Text = "Window1";
+            this.Load += new System.EventHandler(this.Window1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Wisej.Web.TextBox textBox1;
+        private Wisej.Web.Label label1;
+        private Wisej.Web.Button button1;
+    }
+}
+
---
+++ WebApplication4/Window1.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace WebApplication4
+{
+    public partial class Window1 : Form
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window1_Load(object sender, EventArgs e)
+        {
+            Wisej.Base.Cookie c = Wisej.Web.Application.Cookies.Get("test");
+            if (c != null)
+                textBox1.Text = c.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Wisej.Base.Cookie c = Wisej.Web.Application.Cookies.Get("test");
+            //if (c == null)
+                Wisej.Web.Application.Cookies.Add("test", textBox1.Text);
+            //else
+            //    c.Value = textBox1.Text;
+        }
+    }
+}
